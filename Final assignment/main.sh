b701022701bcb0af45ac81e61312c6241bf86d93
Marijn <51094560+Marijndl@users.noreply.github.com>
2025-03-17 16:20:57 +0100

    Add optuna

--- Final assignment/main.sh
+++ Final assignment/main.sh
@@ -1,5 +1,16 @@
 wandb login
 
+# Create a virtual environment in home directory (if not already created)
+export VENV_DIR="$HOME/venv_optuna"
+if [ ! -d "$VENV_DIR" ]; then
+    python3 -m venv $VENV_DIR
+    source $VENV_DIR/bin/activate
+    pip install --upgrade pip
+    pip install -r requirements.txt
+else
+    source $VENV_DIR/bin/activate
+fi
+
 python3 train.py \
     --data-dir ./data/cityscapes \
     --batch-size 64 \
