Fix loss calculation

Final assignment/train.py
@@ -72,8 +72,8 @@ def get_args_parser():
     parser.add_argument("--seed", type=int, default=42, help="Random seed for reproducibility")
     parser.add_argument("--experiment-id", type=str, default="unet-training", help="Experiment ID for Weights & Biases")
     parser.add_argument("--T", type=int, default=2, help="Temperature for smoothing")
-    parser.add_argument("--st-loss", type=int, default=0.25/50000, help="Relative weight of soft target loss (teacher loss).")
-    parser.add_argument("--ce-loss", type=int, default=1, help="Relative weight of cross entropy loss (training loss).")
+    parser.add_argument("--st-loss", type=float, default=0.25, help="Relative weight of soft target loss (teacher loss).")
+    parser.add_argument("--ce-loss", type=float, default=1, help="Relative weight of cross entropy loss (training loss).")
 
     return parser
 
@@ -225,7 +225,8 @@ def main(args):
             soft_targets = nn.functional.softmax(teacher_logits / args.T, dim=1)
             soft_prob = nn.functional.log_softmax(student_logits / args.T, dim=1)
 
-            soft_targets_loss = torch.sum(soft_targets * (soft_targets.log() - soft_prob)) / soft_prob.size()[0] * (args.T**2)
+            # soft_targets_loss = torch.sum(soft_targets * (soft_targets.log() - soft_prob)) / soft_prob.size()[0] * (args.T**2)
+            soft_targets_loss = nn.functional.kl_div(soft_prob, soft_targets, reduction="batchmean") * (args.T**2)
             label_loss = criterion(student_logits, labels)
 
             loss = args.st_loss * soft_targets_loss + args.ce_loss * label_loss