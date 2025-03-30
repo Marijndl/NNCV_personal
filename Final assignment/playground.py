bb4aa8e408fee4ccde9a7b6e98a8acbe23779198
Marijn <51094560+Marijndl@users.noreply.github.com>
2025-03-30 14:25:32 +0200

    Update playground.py

--- Final assignment/playground.py
+++ Final assignment/playground.py
@@ -24,8 +24,37 @@ import segmentation_models_pytorch as smp
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(f"Using device: {device}")
 
-model = smp.Unet('resnet34', encoder_weights='imagenet')
+# Define the model
+model = smp.DeepLabV3Plus(
+    encoder_name="mobilenet_v2",  
+    encoder_weights="imagenet",  
+    decoder_channels=512,  
+    decoder_atrous_rates=(6, 12, 18),  
+    in_channels=3,  
+    classes=19,  
+    activation=None,  
+    aux_params=dict(
+        pooling="avg",  
+        dropout=0.2,  
+        activation="softmax2d",  
+        classes=19  
+    )
+)
 
-# After training your model, save it to a directory
-model.save_pretrained('./my_model')
 
+from prettytable import PrettyTable
+
+def count_parameters(model):
+    table = PrettyTable(["Modules", "Parameters"])
+    total_params = 0
+    for name, parameter in model.named_parameters():
+        if not parameter.requires_grad:
+            continue
+        params = parameter.numel()
+        table.add_row([name, params])
+        total_params += params
+    print(table)
+    print(f"Total Trainable Params: {total_params}")
+    return total_params
+    
+count_parameters(model)
