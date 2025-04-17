7f190bbff7a60e9f65fb184ba63c4c23e5784adb
Marijn <51094560+Marijndl@users.noreply.github.com>
2025-04-17 08:35:40 +0200

    Update bash scripts

--- Final assignment/main.sh
+++ Final assignment/main.sh
@@ -1,5 +1,16 @@
 wandb login
 
+# Create a virtual environment in home directory (if not already created)
+export VENV_DIR="$HOME/venv_quantization"
+if [ ! -d "$VENV_DIR" ]; then
+    python3 -m venv $VENV_DIR
+    source $VENV_DIR/bin/activate
+    pip install --upgrade pip
+    pip install fvcore kornia segmentation_models_pytorch thop torch_tensorrt
+else
+    source $VENV_DIR/bin/activate
+fi
+
 python3 train.py \
     --data-dir ./data/cityscapes \
     --batch-size 64 \
--- Final assignment/quant.sh
+++ Final assignment/quant.sh
@@ -1,5 +1,16 @@
 wandb login
 
+# Create a virtual environment in home directory (if not already created)
+export VENV_DIR="$HOME/venv_quantization"
+if [ ! -d "$VENV_DIR" ]; then
+    python3 -m venv $VENV_DIR
+    source $VENV_DIR/bin/activate
+    pip install --upgrade pip
+    pip install fvcore kornia segmentation_models_pytorch thop torch_tensorrt
+else
+    source $VENV_DIR/bin/activate
+fi
+
 python3 -c "import modelopt.torch.quantization.extensions as ext; ext.precompile()"
 
 python3 static_quantization.py \
